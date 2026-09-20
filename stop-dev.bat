0c5b1993aba03288194f43f2e752c467bb8ed5c4
Shahidcodee <139958934+Shahidcodee@users.noreply.github.com>
2026-09-20 22:50:53 +0530

    Delete stop-dev.bat

--- stop-dev.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Stopping existing Node and Vite processes on ports 8787 and 5173...
-for /f "tokens=5" %%a in ('netstat -aon ^| findstr :8787') do taskkill /F /PID %%a 2>nul
-for /f "tokens=5" %%a in ('netstat -aon ^| findstr :5173') do taskkill /F /PID %%a 2>nul
-echo Done! You can now run .\start-dev.bat again.
